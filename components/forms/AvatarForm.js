82ec249e56b7eb1f2863793494aef0481556d31f
Mark <m.siegrist92@gmail.com>
2020-11-22 10:01:48 -0500

    live update of user customize form implemented - props, funcs parent to child

--- components/forms/AvatarForm.js
+++ components/forms/AvatarForm.js
@@ -1,40 +1,19 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 
-const AvatarForm = () => {
+const AvatarForm = (props) => {
 
   //process.env.NEXT_PUBLIC_DEV_API + '/user/' + "5fb4082408eb0123095db14e" + '/avatar'
 
-  const [file, setFile] = useState();
-
-  const uploadAvatar = event => {
-    event.preventDefault();
-    const data = new FormData();
-    data.append("file", file);
-    //replace ID with sessionStorage .token(decoded with secret)
-    //or just allow user access to their ID in the client - is that safe?
-    axios.post(process.env.NEXT_PUBLIC_DEV_API +
-        '/user/' + "5fb4082408eb0123095db14e" + '/avatar', data,
-      {
-        headers: {
-        "Content-Type": "multipart/form-data"
-      }
-    }).then((res) => {
-      console.log(res)
-    }).catch((err) => {
-      console.log(err);
-    })
-  }
-
   return (
     <div>
-      <form onSubmit={(e) => uploadAvatar(e)}
+      <form onSubmit={(e) => props.uploadAvatar(e, props.img)}
         encType='multipart/form-data'
         accept='image/jpeg'
       >
         <input id='file' required
           type='file' name='avatar'
-          onChange={(event) => setFile(event.target.files[0])}
+          onChange={(event) => props.setAvatar(event.target.files[0])}
         />
         <input type='submit' />
       </form>
--- pages/user.js
+++ pages/user.js
@@ -40,10 +40,34 @@ const User = () => {
       })
   }
 
+  const uploadAvatar = (event, avatar) => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append("file", avatar);
+    //replace ID with sessionStorage .token(decoded with secret)
+    //or just allow user access to their ID in the client - is that safe?
+    axios.post(process.env.NEXT_PUBLIC_DEV_API +
+        '/user/' + "5fb4082408eb0123095db14e" + '/avatar', data,
+      {
+        headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    }).then((res) => {
+      setDBAvatar(res.data.avatar);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+
   return (
     <div>
       <AvatarInfo img_src={DBAvatar} />
-      <AvatarForm />
+      <AvatarForm
+        setAvatar={setAvatar}
+        uploadAvatar={uploadAvatar}
+        img={avatar}
+       />
       <SigInfo sig={DBSig}/>
       <SigForm
         updateSig={setSig}
